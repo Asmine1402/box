0c9b1344a29de8e1f63388c2c2ed1b91813fb260
Miriasme <johndoe@example.com>
2026-07-27 15:38:44 +0300

    feat(Controller): add new controller for /hello

--- src/main/java/com/hei/course/controller/EntierController.java
+++ src/main/java/com/hei/course/controller/EntierController.java
@@ -1,17 +1,15 @@
 package com.hei.course.controller;
 
+import java.util.Random;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
-
 @RestController
 public class EntierController {
-    @GetMapping("/stored_int")
-    public Integer getEntier(){
+  @GetMapping("/stored_int")
+  public Integer getEntier() {
 
-        Random rand = new Random();
-       return rand.nextInt(10);
-
-    }
+    Random rand = new Random();
+    return rand.nextInt(10);
+  }
 }
