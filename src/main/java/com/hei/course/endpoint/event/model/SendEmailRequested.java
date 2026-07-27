92a3f2de1cda9cf7c3e487cd950ddb71c9a339ab
Miriasme <johndoe@example.com>
2026-07-27 18:48:52 +0300

    fix(Event): commente all code to see bug in build

--- src/main/java/com/hei/course/endpoint/event/model/SendEmailRequested.java
+++ src/main/java/com/hei/course/endpoint/event/model/SendEmailRequested.java
@@ -1,32 +1,32 @@
-package com.hei.course.endpoint.event.model;
-
-import java.time.Duration;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder(toBuilder = true)
-@Data
-@EqualsAndHashCode(callSuper = false)
-@ToString
-public class SendEmailRequested extends PojaEvent {
-  private String to;
-  private String subject;
-  private String salutation;
-  private String body;
-
-  @Override
-  public Duration maxConsumerDuration() {
-    return Duration.ofSeconds(45);
-  }
-
-  @Override
-  public Duration maxConsumerBackoffBetweenRetries() {
-    return Duration.ofSeconds(30);
-  }
-}
+//package com.hei.course.endpoint.event.model;
+//
+//import java.time.Duration;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//import lombok.NoArgsConstructor;
+//import lombok.ToString;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder(toBuilder = true)
+//@Data
+//@EqualsAndHashCode(callSuper = false)
+//@ToString
+//public class SendEmailRequested extends PojaEvent {
+//  private String to;
+//  private String subject;
+//  private String salutation;
+//  private String body;
+//
+//  @Override
+//  public Duration maxConsumerDuration() {
+//    return Duration.ofSeconds(45);
+//  }
+//
+//  @Override
+//  public Duration maxConsumerBackoffBetweenRetries() {
+//    return Duration.ofSeconds(30);
+//  }
+//}
