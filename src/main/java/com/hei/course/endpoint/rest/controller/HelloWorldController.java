e87dfd188ffa593ae4da16e92ce44ac49f8bf4a6
Miriasme <johndoe@example.com>
2026-07-27 16:31:14 +0300

    fix(CI/CD): formate code indentation

--- src/main/java/com/hei/course/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/hei/course/endpoint/rest/controller/HelloWorldController.java
@@ -1,9 +1,8 @@
 package com.hei.course.endpoint.rest.controller;
 
-import java.util.List;
-
 import com.hei.course.endpoint.event.EventProducer;
 import com.hei.course.endpoint.event.model.SendEmailRequested;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,20 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-    private final EventProducer<SendEmailRequested> eventProducer;
+  private final EventProducer<SendEmailRequested> eventProducer;
 
-    @GetMapping("/hello")
-    @SneakyThrows
-    public String helloWorld(@RequestParam String to,
-                             @RequestParam String subject,
-                             @RequestParam(defaultValue = "Bonjour,") String salutation,
-                             @RequestParam String body) {
-        var event = SendEmailRequested.builder().to(to)
-                        .subject(subject)
-                                .salutation(salutation)
-                                        .body(body).
-                build();
-        eventProducer.accept(List.of(event));
-        return "Email sent successfully";
-    }
+  @GetMapping("/hello")
+  @SneakyThrows
+  public String helloWorld(
+      @RequestParam String to,
+      @RequestParam String subject,
+      @RequestParam(defaultValue = "Bonjour,") String salutation,
+      @RequestParam String body) {
+    var event =
+        SendEmailRequested.builder()
+            .to(to)
+            .subject(subject)
+            .salutation(salutation)
+            .body(body)
+            .build();
+    eventProducer.accept(List.of(event));
+    return "Email sent successfully";
+  }
 }
