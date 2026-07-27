901f362b271e4a814057cd97ad73404a6b6dfddb
Miriasme <johndoe@example.com>
2026-07-27 15:39:35 +0300

    feat(Controller): add new controller for /hello

---
+++ src/main/java/com/hei/course/endpoint/rest/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.hei.course.endpoint.rest.controller;
+
+import java.util.List;
+
+import com.hei.course.endpoint.event.EventProducer;
+import com.hei.course.endpoint.event.model.SendEmailRequested;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class HelloWorldController {
+    private final EventProducer<SendEmailRequested> eventProducer;
+
+    @GetMapping("/hello")
+    @SneakyThrows
+    public String helloWorld(@RequestParam String to) {
+        var event = SendEmailRequested.builder().to(to).build();
+        eventProducer.accept(List.of(event));
+        return "... world!";
+    }
+}
