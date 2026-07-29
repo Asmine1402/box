d2bf3b127730474383311e6150147d9134e571b8
Miriasme <johndoe@example.com>
2026-07-29 11:35:42 +0300

    feat(HelloWorldService): add endpoint /hello to return file with hello world

--- src/main/java/com/hei/course/service/HelloWorldService.java
+++ src/main/java/com/hei/course/service/HelloWorldService.java
@@ -2,12 +2,11 @@ package com.hei.course.service;
 
 import static java.io.File.createTempFile;
 
+import com.hei.course.file.bucket.BucketComponent;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Duration;
-
-import com.hei.course.file.bucket.BucketComponent;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
@@ -15,22 +14,22 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class HelloWorldService {
-    private final BucketComponent bucketComponent;
+  private final BucketComponent bucketComponent;
 
-    @SneakyThrows
-    public String uploadHelloWorldMessage(String name) {
-        var fileSuffix = ".txt";
-        var filePrefix = "hello-world-" + name;
-        var bucketKey = filePrefix + fileSuffix;
-        var fileToUpload = createTempFile(filePrefix, fileSuffix);
-        writeMessageIntoFile("Hello World " + name + " !", fileToUpload);
-        bucketComponent.upload(fileToUpload, bucketKey);
-        return bucketComponent.presign(bucketKey, Duration.ofMinutes(5)).toString();
-    }
+  @SneakyThrows
+  public String uploadHelloWorldMessage(String name) {
+    var fileSuffix = ".txt";
+    var filePrefix = "hello-world-" + name;
+    var bucketKey = filePrefix + fileSuffix;
+    var fileToUpload = createTempFile(filePrefix, fileSuffix);
+    writeMessageIntoFile("Hello World " + name + " !", fileToUpload);
+    bucketComponent.upload(fileToUpload, bucketKey);
+    return bucketComponent.presign(bucketKey, Duration.ofMinutes(5)).toString();
+  }
 
-    private void writeMessageIntoFile(String message, File file) throws IOException {
-        FileWriter writer = new FileWriter(file);
-        writer.write(message);
-        writer.close();
-    }
+  private void writeMessageIntoFile(String message, File file) throws IOException {
+    FileWriter writer = new FileWriter(file);
+    writer.write(message);
+    writer.close();
+  }
 }
